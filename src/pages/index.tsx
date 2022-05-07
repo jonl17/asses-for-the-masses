33521c7134f7c28426df2b9db4e0387f758632ef
Jón Gabríel Lorange <jonl17@ru.is>
2022-05-07 11:37:14 +0000

    animate the asses

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import LeftAss from '../components/Icons/LeftAss'
+import RightAss from '../components/Icons/RightAss'
 import Logo from '../components/Icons/Logo'
 
 export default function Home() {
   return (
-    <div className='min-h-screen bg-gradient-pink grid place-items-center'>
-      <Logo className='w-full p-5 lg:p-0 mb-24' />
+    <div className='min-h-screen bg-gradient-pink grid place-items-center overflow-hidden relative'>
+      <Logo className='w-full p-5 lg:p-0 mb-24 relative' />
+      <LeftAss className='absolute left-0 top-0 -translate-x-full animate-slide-in-left' />
+      <RightAss className='absolute right-0 top-0 translate-x-full animate-slide-in-right' />
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ module.exports = {
       backgroundImage: {
         'gradient-pink': 'linear-gradient(180deg, #EA75E2 0%, #639DC7 100%);',
       },
+      keyframes: {
+        slideInLeft: {
+          from: { transform: 'translateX(-100%)' },
+          to: { transform: 'translateX(0)' },
+        },
+        slideInRight: {
+          from: { transform: 'translateX(100%)' },
+          to: { transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        'slide-in-left': 'slideInLeft 1s 0s forwards',
+        'slide-in-right': 'slideInRight 1s 1s forwards',
+      },
     },
   },
   plugins: [],
